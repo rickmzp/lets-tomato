77cddb203227d4e291e0c40e6b1f55508c30b2f1
Rick Martínez <rickmzp@gmail.com>
2020-07-30 22:08:46 -0400

    Count down from 25 minutes
    Co-authored-by: Scott Bartell <me@scottbartell.com>

--- cypress/integration/pomodoro.spec.js
+++ cypress/integration/pomodoro.spec.js
@@ -1,4 +1,18 @@
-it('renders a 25 minute timer', () => {
+it('counts down a timer from 25 minutes to 0 minutes', () => {
+  cy.clock()
   cy.visit('index.html')
-  cy.contains('25:00')
+
+  cy.get('#time-left').should('have.text', '25:00')
+
+  cy.tick(1000)
+  cy.get('#time-left').should('have.text', '24:59')
+
+  cy.tick(24 * 60 * 1000)
+  cy.get('#time-left').should('have.text', '0:59')
+
+  cy.tick(59 * 1000)
+  cy.get('#time-left').should('have.text', '0:00')
+
+  cy.tick(1000)
+  cy.get('#time-left').should('have.text', '0:00')
 });
